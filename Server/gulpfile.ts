53aa7b56af253cda437904d2458b10d56b99ad3a
Rida Sarwar <ridarocks12@yahoo.com>
2016-02-28 02:16:27 +0500

    Update gulpfile.ts

--- Server/gulpfile.ts
+++ Server/gulpfile.ts
@@ -6,14 +6,19 @@ let gulp = require('gulp');
 let ts = require('gulp-typescript');
 let rimraf = require('gulp-rimraf');
 let nodemon = require('gulp-nodemon');
+let PATHS = {
+    src: 'src/**/*.ts'
+};
 
 gulp.task('clearBuildDir', function(){
     return gulp.src('build').pipe(rimraf());
 });
 
 gulp.task('buildServer' ,['clearBuildDir'], function () {
-    var tScriptResult = gulp.src('src/**/*.ts')
-        .pipe(ts({module: 'CommonJS'}));
+      var typescript = require('gulp-typescript');
+    var tscConfig = require('./tsconfig.json');
+    var tScriptResult = gulp.src(PATHS.src)
+       .pipe(typescript(tscConfig.compilerOptions));
     return tScriptResult.js.pipe(gulp.dest('build/'));
 });
 
